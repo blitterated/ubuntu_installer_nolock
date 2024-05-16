80ad1310635407500d30f0cd96c3a2abf38b5758
blitterated <blitterated@protonmail.com>
2024-05-15 20:59:15 -0500

    Some code and readability cleanup

--- pico_mouse_jiggler.py
+++ pico_mouse_jiggler.py
@@ -3,18 +3,37 @@ from adafruit_hid.mouse import Mouse
 from time import sleep
 import math
 
-m = Mouse(usb_hid.devices)
+MOUSE = Mouse(usb_hid.devices)
+
+# Set the size of the circle traced
+RADIUS = 2.5
+
+# Set how long it takes to trace a circle in seconds
+DRAW_TIME = 1
+
+# Set the delay between tracing circles in seconds
+DELAY = 10
+
+def build_circle_coords():
+    # Build a list of radians for calculating points along the circumference
+    radians = list(map(math.radians, range(360)))
+
+    # Build a list of coordinate tuples that trace the circumference
+    circle_coords = [(int(math.cos(r) * RADIUS), int(math.sin(r) * RADIUS)) for r in radians]
+
+    return circle_coords
+
+COORDS = build_circle_coords()
+SLEEP_TIME = DRAW_TIME / len(COORDS)
+NO_WHEEL = 0
 
 while True:
     print("\nJiggling\n")
 
-    radius = 2.5
-    radians = list(map(math.radians, range(360)))
-    circle_coords = [(int(math.cos(r) * radius), int(math.sin(r) * radius)) for r in radians]
-    for x, y in circle_coords:
+    for x, y in COORDS:
         print(f"x:{x}, y:{y}")
-        m.move(int(x), int(y), 0)
-        sleep(.005)
+        MOUSE.move(x, y, NO_WHEEL)
+        sleep(SLEEP_TIME)
 
     print("\nSleeping")
-    sleep(10)
+    sleep(DELAY)
